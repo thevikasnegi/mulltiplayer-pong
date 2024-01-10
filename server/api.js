fe91a6e2023a5c8af17da82c19655510808cd326
Vikas Negi <negivikas201@gmail.com>
2024-01-11 01:26:29 +0530

    a few changes

--- server/api.js
+++ server/api.js
@@ -3,10 +3,10 @@ const path = require('path');
 
 const api = express();
 
-api.use(express.static(path.join(__dirname, 'public')));
+api.use(express.static(path.join(__dirname, "..","client",'build')));
 
 api.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+    res.sendFile(path.join(__dirname,"..","client", 'build', 'index.html'));
   });
 
 module.exports = api;
