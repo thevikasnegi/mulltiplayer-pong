27ba6f50604b1b83db5feefde8a14b35dd7777cf
Vikas Negi <72794288+thevikasnegi@users.noreply.github.com>
2025-07-27 20:44:09 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ const socketServer = io(httpServer);
 
 const sockets = require('./socket');
 
-const PORT = 3000;
+const PORT = 3001;
 httpServer.listen(PORT);
 console.log(`Listening on port ${PORT}...`);
 
